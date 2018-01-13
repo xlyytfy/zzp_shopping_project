eb765efd8356a2fcbbdad05d558d55ce4b2ece49
zzp <zzp@test.com>
2018-01-13 20:59:56 +0800

    实现内容分类管理

--- taotao-manager-web/src/main/java/com/taotao/manager/controller/ImageController.java
+++ taotao-manager-web/src/main/java/com/taotao/manager/controller/ImageController.java
@@ -1,25 +1,24 @@
 package com.taotao.manager.controller;
 
 import java.awt.image.BufferedImage;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 import org.apache.commons.lang3.StringUtils;
-import org.csource.common.MyException;
 import org.csource.fastdfs.ClientGlobal;
 import org.csource.fastdfs.StorageClient;
 import org.csource.fastdfs.StorageServer;
 import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taotao.common.pojo.PicUploadResult;
 
 @Controller
@@ -32,6 +31,81 @@ public class ImageController {
 	//声明可用的文件类型
 	private String[] TYPE = { ".jpg", ".jpeg", ".png", ".bmp", ".gif" };
 	
+	//使用Jackson工具类把对象转换为json数据
+	private static final ObjectMapper mapper = new ObjectMapper();
+	
+	
+	@RequestMapping(method = RequestMethod.POST,produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String upload(MultipartFile uploadFile) throws Exception{
+		PicUploadResult picUploadResult = new PicUploadResult();
+		//默认设置上传失败
+		picUploadResult.setError(1);
+		
+		//默认判断标识为fasle
+		boolean flag = false;
+		
+		//1:文件类型判断
+		for (String suffix : TYPE) {
+			//如果文件类型匹配
+			if(StringUtils.endsWithIgnoreCase(uploadFile.getOriginalFilename(), suffix)){
+				flag = true;
+				break;
+			}
+		}
+		
+		if(!flag){
+			//如果类型不存在则返回错误
+			return mapper.writeValueAsString(picUploadResult);
+		}
+		
+		//重置标识
+		flag = false;
+		
+		//2：文件内容判读
+		try {
+			BufferedImage image = ImageIO.read(uploadFile.getInputStream());
+			if(null != image){
+				picUploadResult.setHeight(String.valueOf(image.getHeight()));
+				picUploadResult.setWidth(String.valueOf(image.getWidth()));
+				flag = true;
+			}
+		} catch (Exception e) {
+			//如果catch异常或者image = null flag = false
+		}
+		
+		if(!flag){
+			//没有获取到宽和高
+			return mapper.writeValueAsString(picUploadResult);
+		}
+		
+		//执行文件上传
+		//初始化服务器连接配置信息
+		//ClientGlobal.init("classpath:tracker.properties");
+		ClientGlobal.init(System.getProperty("user.dir")+"/src/main/resources/tracker.properties");
+		//创建TrackerClient
+		TrackerClient trackerClient = new TrackerClient();
+		//创建trackerServer
+		TrackerServer trackerServer = trackerClient.getConnection();
+		//创建storageServer
+		StorageServer storageServer = null;
+		//创建StorageClient
+		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+		/**
+		 * file_buff:文件字节流
+		 * file_ext_name：文件后缀名
+		 * meta_list：null
+		 */
+		String[] file = storageClient.upload_file(uploadFile.getBytes(),StringUtils.substringAfterLast(uploadFile.getOriginalFilename(), "."), null);
+		
+		//拼接UR地址：http://192.168.37.161/group1/M00/00/00/wKgloVpQP8eACKiaAAM8-p7VfAU696_big.jpg
+		picUploadResult.setUrl(TAOTAO_IMAGE_URL+file[0]+"/"+file[1]);
+		//设置上传成功
+		picUploadResult.setError(0);
+		return mapper.writeValueAsString(picUploadResult);
+	}
+	
+	
 	/**
 	 * 图片上传
 	 * @return
@@ -39,7 +113,7 @@ public class ImageController {
 	 * @throws IOException 
 	 * @throws FileNotFoundException 
 	 */
-	@RequestMapping(method = RequestMethod.POST)
+	/*@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public PicUploadResult upload(MultipartFile uploadFile) throws Exception{
 		PicUploadResult picUploadResult = new PicUploadResult();
@@ -95,11 +169,11 @@ public class ImageController {
 		StorageServer storageServer = null;
 		//创建StorageClient
 		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
-		/**
+		*//**
 		 * file_buff:文件字节流
 		 * file_ext_name：文件后缀名
 		 * meta_list：null
-		 */
+		 *//*
 		String[] file = storageClient.upload_file(uploadFile.getBytes(),StringUtils.substringAfterLast(uploadFile.getOriginalFilename(), "."), null);
 		
 		//拼接UR地址：http://192.168.37.161/group1/M00/00/00/wKgloVpQP8eACKiaAAM8-p7VfAU696_big.jpg
@@ -107,6 +181,6 @@ public class ImageController {
 		//设置上传成功
 		picUploadResult.setError(0);
 		return picUploadResult;
-	}
+	}*/
 
 }
--- taotao-manager-web/src/main/java/com/taotao/manager/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/manager/controller/ItemController.java
@@ -3,8 +3,12 @@ package com.taotao.manager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.taotao.common.pojo.TaoResult;
 import com.taotao.manager.pojo.Item;
 import com.taotao.manager.pojo.ItemDesc;
 import com.taotao.manager.service.ItemDescService;
@@ -20,11 +24,31 @@ public class ItemController {
 	@Autowired
 	private ItemDescService itemDescService;
 	
-	@RequestMapping
+	/**
+	 * 新增商品
+	 * @param item
+	 * @param desc
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public String addItem(Item item,String desc){
 		//保存商品
 		itemService.saveItem(item,desc);
 		return "success";
+	} 
+	
+	/**
+	 * 获取商品列表
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public TaoResult queryItemsforPage(@RequestParam(value="page",defaultValue="1") Integer page,@RequestParam(value="rows",defaultValue="30") Integer rows){
+		//保存商品
+		TaoResult<Item> result = itemService.queryItemsforPage(page,rows);
+		return result;
 	} 
 }
