0ded312e73acb0453ec291d2efd384271d3676f6
zzp <zzp@test.com>
2018-01-14 10:54:36 +0800

    添加spring-test单元测试包

--- taotao-manager/taotao-manager-service/src/test/java/com/taotao/service/test/TaotaoServiceTest.java
+++ taotao-manager/taotao-manager-service/src/test/java/com/taotao/service/test/TaotaoServiceTest.java
@@ -1,7 +1,16 @@
 package com.taotao.service.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:spring/applicationContext-*.xml"})
 public class TaotaoServiceTest {
-
+	
+	@Test
+	public void test(){
+		
+	}
 }
